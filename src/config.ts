75b909a92ea060892585c1ae0557c4343be589b8
Josh Helman <joshhelman@hotmail.co.uk>
2024-04-20 11:44:54 +0100

    Roles config

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,21 @@ export const colours = {
   default: 13724201,
   log: 3847248,
 };
+
+export const roles = {
+  region_roles: {
+    north_america: {
+      name: "North America",
+      role_id: 1047157742831423549,
+      role_emoji: { name: "AMERICA", id: 1212487815645106227 },
+    },
+    europe: {
+      name: "Europe",
+      role_id: 1047157818999963648,
+    },
+    asia: { role_id: 1047158446602063913 },
+    south_america: { role_id: 1212018192516517958 },
+    oceania: { role_id: 1212018262452469820 },
+    africa: { role_id: 1212018320359030794 },
+  },
+};
