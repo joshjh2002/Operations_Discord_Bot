5e5510d390ce1af6220ff847b59aaa62b57daeae
joshjh2002 <joshhelman@hotmail.co.uk>
2024-04-20 12:46:11 +0100

    Added Firestore Database Connection

---
+++ src/firebase/functions.ts
@@ -0,0 +1,46 @@
+import { db } from "../firebase/firebaseInit";
+import { getDoc, setDoc, doc, updateDoc, collection } from "firebase/firestore";
+
+export const getDocument = async (collection: string, document: string) => {
+  const data = doc(db, collection, document);
+  const docData = (await getDoc(data)).data();
+  return docData;
+};
+
+export const getRecord = async (
+  collection: string,
+  document: string,
+  record: string
+) => {
+  const data = doc(db, collection, document);
+  const docData = (await getDoc(data)).data();
+  return docData?.[record] || "Record not found.";
+};
+
+export const setRecord = async (
+  collection: string,
+  document: string,
+  record: string,
+  data: any
+) => {
+  const docRef = doc(db, collection, document);
+  await updateDoc(docRef, {
+    [record]: data,
+  });
+};
+
+export const setDocument = async (
+  collection: string,
+  document: string,
+  data: any
+) => {
+  await setDoc(doc(db, collection, document), data);
+};
+
+export const updateDocument = async (
+  collection: string,
+  document: string,
+  data: any
+) => {
+  await updateDoc(doc(db, collection, document), data);
+};
